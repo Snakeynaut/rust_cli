6664e9e9241e02dec5d7e5570a98ba28846adf7c
aminf <snakeyna@student.ubc.ca>
2023-06-11 23:19:52 -0700

    Added the ability to read arguments from the command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filepath = &args[2];
+    let current_direc = env::current_dir().expect("Unable to read current directory")
+                                    .to_str().expect("Unable to open directory").to_string();
+
+    println!("Searching for {}", query);
+    println!("In file {}", filepath);
+    println!("Current director is {:?}", current_direc);
 }
