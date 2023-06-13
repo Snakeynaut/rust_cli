297ee6da48e10286d8f7c42b753295df13d46d36
aminf <snakeyna@student.ubc.ca>
2023-06-13 00:14:19 -0700

    Used a struct to store command line configurations rather than variables

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,34 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filepath = &args[2];
+
+    let config = Config::new(&args);
+
     let current_direc = env::current_dir()
         .expect("Unable to read current directory")
         .to_str().expect("Unable to open directory").to_string();
 
-    println!("Searching for {}", query);
-    println!("In file {}", filepath);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filepath);
     println!("Current directory is {:?}", current_direc);
 
-    let contents = fs::read_to_string(filepath)
+    let contents = fs::read_to_string(config.filepath)
         .expect("Should have been able to read file");
 
     println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    filepath: String,
+}
+
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filepath = args[2].clone();
+
+        Config {query, filepath};
+    }
 }
